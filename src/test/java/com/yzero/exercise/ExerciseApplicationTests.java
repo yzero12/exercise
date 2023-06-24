741c1e2258b35d889816ed6521d411f751d9f08d
yzero12 <2358702934@qq.com>
2023-06-24 11:00:34 +0800

    2023.6.24新建

---
+++ src/test/java/com/yzero/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzero.exercise;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/yzero/exercise/test/MyTest.java
@@ -0,0 +1,21 @@
+package com.yzero.exercise.test;
+
+import com.yzero.exercise.method.MyCustomMethod;
+import org.junit.Test;
+
+/**
+ * ClassName: MyTest
+ * Package: com.yzero.exercise.test
+ * Description:
+ *
+ * @author zjzstart
+ * @create 2023-06-24 10:50
+ */
+public class MyTest {
+
+    @Test
+    public void test(){
+        MyCustomMethod.getRepeatCharNum("fahkgfhakghaighaighae");
+    }
+
+}
